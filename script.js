d27c43761c14a819d72cc62dafa279085dfa0426
Nikita Lobanov <114199158+nikitalobanov12@users.noreply.github.com>
2023-01-22 10:44:27 -0800

    vidpotential v1 website
    still need to add a contact form, make it mobile friendly and style it to the clients liking

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper(".swiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: "true",
   },
-
 });
