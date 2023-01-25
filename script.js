bb22123807069e3847460f5bb14d7b11bd0aaada
Nikita Lobanov <114199158+nikitalobanov12@users.noreply.github.com>
2023-01-25 10:19:04 -0800

    Add files via upload
    added scroll animations

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: "true",
+const videos = document.querySelectorAll(".intro-video");
+
+// hook that takes a classname or an element as a parameter and returns whether or not it is visible
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
+    });
   },
+  {
+    // threshold: 1, //the percentage of the content that needs to be on the screen
+    rootMargin: '-100px' //the margin for the animation to start, you can use negative numbers to shrink the container and positive numbers to increase the container
+  }
+);
+
+videos.forEach((video) => {
+  observer.observe(video);
 });
