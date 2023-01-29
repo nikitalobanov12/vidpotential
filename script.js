37d5c89aba94bd7942c717251833ef0ea1c97dc5
Nikita Lobanov <114199158+nikitalobanov12@users.noreply.github.com>
2023-01-28 17:11:15 -0800

    Add files via upload
    january 28, 2022 version, added animations and made the contact page, need to fully flesh out animations and make it mobile friendly

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const videos = document.querySelectorAll(".intro-video");
 
+const subtitle = document.querySelector(".subtitle");
+const hiddens = document.querySelectorAll(".hidden");
 // hook that takes a classname or an element as a parameter and returns whether or not it is visible
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       entry.target.classList.toggle("show", entry.isIntersecting);
       if (entry.isIntersecting) {
+        subtitle.classList.add("show");
         observer.unobserve(entry.target);
       }
     });
   },
   {
-    // threshold: 1, //the percentage of the content that needs to be on the screen
-    rootMargin: '-100px' //the margin for the animation to start, you can use negative numbers to shrink the container and positive numbers to increase the container
+     threshold: 1, //the percentage of the content that needs to be on the screen
+    // rootMargin: '-500px' //the margin for the animation to start, you can use negative numbers to shrink the container and positive numbers to increase the container
   }
 );
-
-videos.forEach((video) => {
-  observer.observe(video);
+hiddens.forEach((hidden) => {
+  observer.observe(hidden); 
+ 
 });
